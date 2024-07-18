b91235ddc6f713825af219c78154346437c249d2
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-07-18 10:26:34 -0400

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -11,19 +11,46 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
-
 // Create human choice function //
 
 function getHumanChoice() {
     let answer = prompt("Enter rock, paper, or scissors").toLowerCase();
     if (!(answer === "paper" || answer === "rock" || answer === "scissors")) {
-       return getHumanChoice();
+        return getHumanChoice();
     } else {
         return answer;
     } 
 }
 
-console.log(getHumanChoice())
+// Declare score variables //
 
+let humanScore = 0;
+let computerScore = 0;
 
+// Create function for a single round //
+
+function playRound (humanChoice, computerChoice) {
+    if ((humanChoice === "rock" && computerChoice === "scissors") 
+        || (humanChoice === "paper" && computerChoice === "rock") 
+        || (humanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore += 1;
+        console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+    } else if (humanChoice === computerChoice) {
+        humanScore += 1;
+        computerScore += 1;
+        console.log("It's a tie!");
+    } else {
+        computerScore += 1;
+        console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+    }
+    console.log("Your score = " + humanScore + "," + " Computer's score = " + computerScore)
+}
+
+// Declare variables to use as arguments in playRound by calling choice functions//
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+// Pass arguments as parameters //
+
+playRound(humanSelection, computerSelection);
