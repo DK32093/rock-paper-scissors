4efdb24d187e53a7162012b2141869775507090f
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-08-08 15:07:29 -0400

    Update listener including classes to id's

--- javascript.js
+++ javascript.js
@@ -11,33 +11,6 @@ function getComputerChoice() {
     }
 }
 
-// Event listener for button choice
-let choices = document.querySelector(".choices");
-choices.addEventListener("click", (event) => {
-    let target = event.target;
-    if (target.class = "rock") {
-        let humanSelection = "rock";
-        return humanSelection;
-    } else if (target.class = "paper") {
-        let humanSelection = "paper";
-        return humanSelection;
-    } else {
-        let humanSelection = "scissors";
-        return humanSelection;
-    }
-});
-
-// Create human choice function that won't accept the wrong answer//
-function getHumanChoice() {
-    let answer = prompt("Enter rock, paper, or scissors").toLowerCase();
-    if (!(answer === "paper" || answer === "rock" || answer === "scissors")) {
-        alert("Check your spelling ;)");
-        return getHumanChoice();
-    } else {
-        return answer;
-    } 
-}
-
 // Create function for a single round //
 function playRound (humanChoice, computerChoice) {
     if ((humanChoice === "rock" && computerChoice === "scissors") 
@@ -56,24 +29,38 @@ function playRound (humanChoice, computerChoice) {
     console.log("Your score = " + humanScore + "," + " Computer's score = " + computerScore)
 }
 
-// Create a function that plays 5 rounds, keeps score, and declares the results //
-function playGame () {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    
-    if (humanScore > computerScore) {
-        console.log("Game over. You won the game!");
-    } else if (computerScore > humanScore) {
-        console.log("Game over. You lost the game!");
-    } else {
-        console.log("Game over. It's a tie! No winner.");
-    }
-}
 
-// Declare global variables and call the playGame function //
 let humanScore = 0;
 let computerScore = 0;
-let gameCount = 0;
 
-playGame();
+// Event listener for button choice
+let choices = document.querySelector(".choices");
+choices.addEventListener("click", (event) => {
+    let target = event.target;
+    if (target.id == "rock") {
+        humanSelection = "rock";
+    } else if (target.id == "paper") {
+        humanSelection = "paper";
+    } else {
+        humanSelection = "scissors";
+    } 
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+});
+
+// Create a function that plays 5 rounds, keeps score, and declares the results //
+// function playGame () {
+//     let humanSelection = getHumanChoice();
+//     let computerSelection = getComputerChoice();
+//     playRound(humanSelection, computerSelection);
+    
+//     if (humanScore > computerScore) {
+//         console.log("Game over. You won the game!");
+//     } else if (computerScore > humanScore) {
+//         console.log("Game over. You lost the game!");
+//     } else {
+//         console.log("Game over. It's a tie! No winner.");
+//     }
+// }
+
+// Declare global variables and call the playGame function //
